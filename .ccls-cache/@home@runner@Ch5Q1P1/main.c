3c85cfec02ba0168218211fa854e990d406aa257
Iain Taylor <iain.taylor.94@gmail.com>
2023-06-26 13:38:55 +0000

    completed program

--- .ccls-cache/@home@runner@Ch5Q1P1/main.c
+++ .ccls-cache/@home@runner@Ch5Q1P1/main.c
@@ -1,6 +1,14 @@
+// Program to calculate the eighth triangular number
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int triangularNumber;
+
+  triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+  printf ("The eighth triangular number is %i\n", triangularNumber);
+  
   return 0;
 }
